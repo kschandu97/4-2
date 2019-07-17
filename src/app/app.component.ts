5fb635dad07fa345f9db47cca6130aa74ceac013
kschandu97 <52074031+kschandu97@users.noreply.github.com>
2019-07-17 09:49:54 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+﻿import { Component, OnInit} from '@angular/core';
+import { Service } from './service';
+import { templateJitUrl } from '@angular/compiler';
+
+@Component({
+    selector: 'app',
+    templateUrl: 'app.component.html'
+})
+
+
+export class AppComponent implements OnInit{
+    constructor(private service: Service) {
+
+    }
+   
+    temp: any[] =[]
+    
+       ngOnInit() {
+        this.service.getData().subscribe(
+            (data: any) => {
+                this.temp = data
+            } 
+        )
+    }
+
+
+  
+    searchID(value) {
+        this.temp.length = 0
+        this.service.getData().subscribe(
+            (data:any)=>{
+                for(let element of data){
+                    if(element.id.toString().toLowerCase().includes(value.toString().toLowerCase())){
+                        this.temp.push(element)
+                    }
+                }
+            }
+        )
+    }
+
+    searchTitle(value) {
+        this.temp.length = 0
+        this.service.getData().subscribe(
+            (data:any)=>{
+                for(let element of data){
+                    if(element.title.toString().toLowerCase().includes(value.toLowerCase())){
+                        this.temp.push(element)
+                    }
+                }
+            }
+        )
+    }
+
+    searchAuthor(value) {
+        this.temp.length = 0
+        this.service.getData().subscribe(
+            (data:any)=>{
+                for(let element of data){
+                    if(element.author.toString().toLowerCase().includes(value.toString().toLowerCase())){
+                        this.temp.push(element)
+                    }
+                }
+            }
+        )
+    }
+
+    searchPublishYear(value) {
+        this.temp.length = 0
+        this.service.getData().subscribe(
+            (data:any)=>{
+                for(let element of data){
+                    if(element.year.toString().toLowerCase().includes(value.toString().toLowerCase())){
+                        this.temp.push(element)
+                    }
+                }
+            }
+        )
+    }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+﻿import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule} from '@angular/forms'
+import { from } from 'rxjs';
+
+@NgModule({
+    imports: [
+        BrowserModule,HttpClientModule,FormsModule
+        
+    ],
+    declarations: [
+        AppComponent
+		],
+    providers: [ ],
+    bootstrap: [AppComponent]
+})
+
+export class AppModule { }
